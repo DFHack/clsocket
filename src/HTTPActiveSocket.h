7ef04f1dfc0e66621dafd52201b48ab77cfff770
Petr Mrázek <peterix@gmail.com>
2012-03-11 13:37:40 +0100

    import original clsocket files

---
+++ src/HTTPActiveSocket.h
@@ -0,0 +1,74 @@
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/* HTTPActiveSocket.h - Active Socket Decleration                            */
+/*                                                                           */
+/* Author : Mark Carrier (mark@carrierlabs.com)                              */
+/*                                                                           */
+/*---------------------------------------------------------------------------*/
+/* Copyright (c) 2010. CarrierLabs, LLC.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * 4. The name "CarrierLabs" must not be used to
+ *    endorse or promote products derived from this software without
+ *    prior written permission. For written permission, please contact
+ *    mark@carrierlabs.com.
+ *
+ * THIS SOFTWARE IS PROVIDED BY MARK CARRIER ``AS IS'' AND ANY
+ * EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL MARK CARRIER OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *----------------------------------------------------------------------------*/
+#ifndef __HTTPACTIVESOCKET_H__
+#define __HTTPACTIVESOCKET_H__
+
+#include <string>
+#include "ActiveSocket.h"
+
+using namespace std;
+
+/// Provides a platform independent class to create an active socket.
+/// An active socket is used to create a socket which connects to a server.  
+/// This type of object would be used when an application needs to send/receive
+/// data from a server. 
+class CHttpActiveSocket : public CActiveSocket {
+
+public:
+	virtual Send(const uint8 *pBuf, size_t bytesToSend);
+	virtual Receive();
+
+private:
+// date
+	string  m_szServer;
+// lastmodified
+	int32   m_nContentLength;
+	string  m_szHost;
+	string  m_szContentType;
+	string  m_bConnection;
+	string  m_szSetCookie;
+	string  m_szLocation;
+	string  m_szCacheControl;
+};
+
+#endif // __HTTPACTIVESOCKET_H__
